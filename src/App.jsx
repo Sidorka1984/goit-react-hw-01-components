93484ed3753014f37f1485e0838d4757710a2776
Olga Ulyanchyk <sidorka.olka@gmail.com>
2021-06-27 14:54:24 +0300

    встановила react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import Container from './components/Container/Container';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Container></Container>
+
+    </div>
+  );
+}
+
+export default App;
